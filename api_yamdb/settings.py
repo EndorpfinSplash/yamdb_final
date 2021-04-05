6fa76f58836ad0aa8a6778058f4d2ff67bf0f848
ANDREY ZINOVICH <andrey.zinovich@consultant.turkcell.com.tr>
2021-04-06 00:36:59 +0300

    fix image

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -91,7 +91,8 @@ DATABASES = {
         # 'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PASSWORD': 'postgres',
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
