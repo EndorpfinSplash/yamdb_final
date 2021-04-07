ae0e090dd710bbbfe5956a91435a218f877056ae
ANDREY ZINOVICH <andrey.zinovich@consultant.turkcell.com.tr>
2021-04-07 23:44:07 +0300

    createsuperuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 sleep 10
 
+echo "from django.contrib.auth.models import User; User.objects.create_superuser($DJANGO_SUPERUSER_USERNAME, $DJANGO_SUPERUSER_EMAIL, $DJANGO_SUPERUSER_PASSWORD)" | python manage.py shell
+
 python manage.py migrate
 python manage.py createcachetable
 python manage.py collectstatic  --noinput
