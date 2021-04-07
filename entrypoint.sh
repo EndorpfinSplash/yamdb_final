160d556275d47cdd7ef6ca95573e1ecd6358fced
ANDREY ZINOVICH <andrey.zinovich@consultant.turkcell.com.tr>
2021-04-08 00:29:37 +0300

    createsuperuser without env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 
 sleep 10
 
-DJANGO_SUPERUSER_USERNAME=$(grep DJANGO_SUPERUSER_USERNAME .env | cut -d '=')
-DJANGO_SUPERUSER_EMAIL=$(grep DJANGO_SUPERUSER_EMAIL .env | cut -d '=')
-DJANGO_SUPERUSER_PASSWORD=$(grep DJANGO_SUPERUSER_PASSWORD .env | cut -d '=')
+#DJANGO_SUPERUSER_USERNAME=$(grep DJANGO_SUPERUSER_USERNAME .env | cut -d '=')
+#DJANGO_SUPERUSER_EMAIL=$(grep DJANGO_SUPERUSER_EMAIL .env | cut -d '=')
+#DJANGO_SUPERUSER_PASSWORD=$(grep DJANGO_SUPERUSER_PASSWORD .env | cut -d '=')
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser(${DJANGO_SUPERUSER_USERNAME}, ${DJANGO_SUPERUSER_EMAIL}, ${DJANGO_SUPERUSER_PASSWORD})" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'fast.optimus@gmail.com', 'secretadmina7')" | python manage.py shell
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser(${DJANGO_SUPERUSER_USERNAME}, ${DJANGO_SUPERUSER_EMAIL}, ${DJANGO_SUPERUSER_PASSWORD})" | python manage.py shell
 
 python manage.py migrate
 python manage.py createcachetable
