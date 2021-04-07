8ea1508044919975cd91c3b9015dc0794d0266aa
ANDREY ZINOVICH <andrey.zinovich@consultant.turkcell.com.tr>
2021-04-08 00:17:10 +0300

    createsuperuser be .env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 
 sleep 10
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser($DJANGO_SUPERUSER_USERNAME, $DJANGO_SUPERUSER_EMAIL, $DJANGO_SUPERUSER_PASSWORD)" | python manage.py shell
+DJANGO_SUPERUSER_USERNAME=$(grep DJANGO_SUPERUSER_USERNAME .env | cut -d '=')
+DJANGO_SUPERUSER_EMAIL=$(grep DJANGO_SUPERUSER_EMAIL .env | cut -d '=')
+DJANGO_SUPERUSER_PASSWORD=$(grep DJANGO_SUPERUSER_PASSWORD .env | cut -d '=')
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser(${DJANGO_SUPERUSER_USERNAME}, ${DJANGO_SUPERUSER_EMAIL}, ${DJANGO_SUPERUSER_PASSWORD})" | python manage.py shell
 
 python manage.py migrate
 python manage.py createcachetable
