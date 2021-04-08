8a0973ae3fb80abeae781e4e9bbe2f78c6b936f6
ANDREY ZINOVICH <andrey.zinovich@consultant.turkcell.com.tr>
2021-04-08 12:04:10 +0300

    createsuperuser2

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,11 @@ sleep 10
 
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser(${DJANGO_SUPERUSER_USERNAME}, ${DJANGO_SUPERUSER_EMAIL}, ${DJANGO_SUPERUSER_PASSWORD})" | python manage.py shell
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin10', 'nafigatort@yandex.ru', 'secretadmina10')" | python manage.py shell
 python manage.py migrate
 python manage.py createcachetable
 python manage.py collectstatic  --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin10', 'nafigatort@yandex.ru', 'secretadmina10')" | python manage.py shell
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin10', 'nafigatort@yandex.ru', 'secretadmina10')" | python manage.py shell
 gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
